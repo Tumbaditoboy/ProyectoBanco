9bd6da2a2fb02ad6526486c45fa8cf682f11f66b
Akane <Akane@DESKTOP-GLP9P0S.mshome.net>
2025-05-08 14:27:08 -0700

    Adición de una clase ENUM para los tipos de usuarios que hay. Adición de los valores en la clase Usuario junto con los get y set. Adición del metodo getAll en la clase UsuarioDAO.

--- banco/src/main/java/mx/itson/banco/entities/Usuario.java
+++ banco/src/main/java/mx/itson/banco/entities/Usuario.java
@@ -4,10 +4,117 @@
  */
 package mx.itson.banco.entities;
 
+
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import mx.itson.banco.utils.TipoUsuario;
+
+
 /**
  *
  * @author Akane
  */
+@Entity
 public class Usuario {
+
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * @return the correo
+     */
+    public String getCorreo() {
+        return correo;
+    }
+
+    /**
+     * @param correo the correo to set
+     */
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    /**
+     * @return the contrasena
+     */
+    public String getContrasena() {
+        return contrasena;
+    }
+
+    /**
+     * @param contrasena the contrasena to set
+     */
+    public void setContrasena(String contrasena) {
+        this.contrasena = contrasena;
+    }
+
+    /**
+     * @return the tipo
+     */
+    public TipoUsuario getTipo() {
+        return tipo;
+    }
+
+    /**
+     * @param tipo the tipo to set
+     */
+    public void setTipo(TipoUsuario tipo) {
+        this.tipo = tipo;
+    }
+
+    /**
+     * @return the saldo
+     */
+    public float getSaldo() {
+        return saldo;
+    }
+
+    /**
+     * @param saldo the saldo to set
+     */
+    public void setSaldo(float saldo) {
+        this.saldo = saldo;
+    }
     
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String nombre;
+    private String correo;
+    private String contrasena;
+    
+    @Enumerated(EnumType.STRING) 
+    private TipoUsuario tipo;
+    
+    private float saldo;
 }
--- banco/src/main/java/mx/itson/banco/persistence/UsuarioDAO.java
+++ banco/src/main/java/mx/itson/banco/persistence/UsuarioDAO.java
@@ -4,10 +4,31 @@
  */
 package mx.itson.banco.persistence;
 
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.criteria.CriteriaQuery;
+import mx.itson.banco.entities.Usuario;
+import mx.itson.banco.utils.HibernateUtil;
+import org.hibernate.Session;
+
 /**
  *
  * @author Akane
  */
 public class UsuarioDAO {
     
+    public static List<Usuario> getAll(){
+        List<Usuario> usuarios = new ArrayList<>();
+        try{
+            Session session = HibernateUtil.getSessionFactory().openSession();
+            CriteriaQuery<Usuario> criteriaQuery = 
+                    session.getCriteriaBuilder().createQuery(Usuario.class);
+            criteriaQuery.from(Usuario.class);
+            
+            usuarios = session.createQuery(criteriaQuery).getResultList();
+        }catch(Exception ex){ 
+            System.err.println("Ocurrió un error: " + ex.getMessage());
+        }
+        return usuarios;
+    }
 }
