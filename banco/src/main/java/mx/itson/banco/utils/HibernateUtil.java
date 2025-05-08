d661be7f76745e84a93d742e9e3e975ba2a4d3ed
Akane <Akane@DESKTOP-GLP9P0S.mshome.net>
2025-05-08 14:03:02 -0700

    Cambios en POM para implementar Hibernate al proyecto. Adición de la configuración para la sesión de Hibernate en la clase HibernateUtil.

--- banco/src/main/java/mx/itson/banco/utils/HibernateUtil.java
+++ banco/src/main/java/mx/itson/banco/utils/HibernateUtil.java
@@ -4,10 +4,42 @@
  */
 package mx.itson.banco.utils;
 
+import org.hibernate.HibernateException;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+
 /**
  *
  * @author Akane
  */
 public class HibernateUtil {
-    
+ private static SessionFactory factory;
+     private static ServiceRegistry serviceRegistry;
+ 
+     static {
+         try{
+             Configuration configuration = new Configuration();
+             configuration.configure();
+             factory = configuration.buildSessionFactory();
+         }catch(HibernateException ex){
+                 System.out.println("No se pudo crear la sesión de Hibernate debido al error: " + ex);
+         }
+     }
+ 
+     public static SessionFactory getSessionFactory() {		
+         return factory;
+     }
+ 
+     public static void setSessionFactory(SessionFactory factory) {
+             HibernateUtil.factory = factory;
+     }
+ 
+     public static ServiceRegistry getServiceRegistry() {
+             return serviceRegistry;
+     }
+ 
+     public static void setServiceRegistry(ServiceRegistry serviceRegistry) {
+             HibernateUtil.serviceRegistry = serviceRegistry;
+     }
 }
